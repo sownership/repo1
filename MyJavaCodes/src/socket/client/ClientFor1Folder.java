736b5558dc7a6147896c9fb752d389cc0a9c61c1
sownership <sownershp@home.my>
2018-02-17 23:25:40 +0900

    naming change

--- MyJavaCodes/src/socket/client/ClientFor1Folder.java
+++ MyJavaCodes/src/socket/client/ClientFor1Folder.java
@@ -12,7 +12,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class Client {
+public class ClientFor1Folder {
 
 	public static void main(String[] args) {
 

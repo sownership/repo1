2de3ac18eeaa7e520d92a0d618889f2032552c66
서효진 <@hihyo00-PC>
2018-04-01 16:30:28 +0900

    algorithm lowerbound, upperbound

---
+++ MyJavaCodes/src/algorithm/UpperBound.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+public class UpperBound {
+
+	public static void main(String[] args) {
+		
+		int[] ia = {1,5,10,15,20};
+		System.out.println(get(ia, 3));
+	}
+
+	private static int get(int[] ia, int t) {
+		
+		int s = 0;
+		int e = ia.length;
+		
+		while(e > s) {
+			int m = (s+e)/2;
+			if(t < ia[m]) {
+				e = m;
+			} else {
+				s = m + 1;
+			}
+		}
+		
+		return s;
+	}
+}

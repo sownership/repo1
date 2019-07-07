e1220f8389c330fbfe59235e9d763e1e8b054779
potter <potter@DESKTOP-1VDN8R6>
2019-07-07 22:49:39 +0900

    diff end

--- MyJavaCodes/src/util/Diff.java
+++ MyJavaCodes/src/util/Diff.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
-public class Diff2 {
+public class Diff {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 
